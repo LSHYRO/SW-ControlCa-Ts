977fd2afa98b86440d406e4f3770d033fac53f6b
Ernesto <ernestoalonsomartinezmartinez@gmail.com>
2024-01-13 00:22:26 -0600

    Creación de archivos de Pantalla para cada tipo de usuario

---
+++ resources/js/Components/profe/OpcionesNavProfe.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+    
+});
+</script>
+
+<template>
+    <a class="block text-black py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+        :href="route('admin.inicio')">
+        <i class="fas fa-home mr-2"></i>Inicio
+    </a>
+    <a class="block text-black py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+        :href="route('admin.tutoresAlum')">
+            <i class=" fas fa-users mr-2"></i>Alumnos y tutores
+    </a>
+    <a class="block text-black py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+        :href="route('admin.materias')">
+        <i class="fa fa-book mr-2"></i>Materias
+    </a>
+    <a class="block text-black py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+        :href="route('admin.clases')">
+        <i class="fas fa-desktop mr-2"></i>Clases
+    </a>
+</template>
---
+++ resources/js/Components/secre/OpcionesNavSecre.vue
@@ -0,0 +1,40 @@
+<script setup>
+defineProps({
+    
+});
+</script>
+
+<template>
+    <a class="block text-black py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+        :href="route('admin.inicio')">
+        <i class="fas fa-home mr-2"></i>Inicio
+    </a>
+    <a class="block text-black py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+        :href="route('admin.tutoresAlum')">
+            <i class=" fas fa-users mr-2"></i>Alumnos y tutores
+    </a>
+    <a class="block text-black py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+        :href="route('admin.profesores')">
+        <i class="fas fa-pencil mr-2"></i>Docentes
+    </a>
+    <a class="block text-black py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+        :href="route('admin.directivos')">
+        <i class="fa fa-briefcase" aria-hidden="true"></i> Directivos
+    </a>
+    <a class="block text-black py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+        :href="route('admin.materias')">
+        <i class="fa fa-book mr-2"></i>Materias
+    </a>
+    <a class="block text-black py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+        :href="route('admin.clases')">
+        <i class="fas fa-desktop mr-2"></i>Clases
+    </a>
+    <a class="block text-black py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+        :href="route('admin.gradosgrupos')">
+        <i class="fas fa-balance-scale mr-2"></i>Grados y grupos
+    </a>
+    <a class="block text-black py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white"
+        :href="route('admin.ciclosperiodos')">
+        <i class="fas fa-calendar mr-2"></i>Ciclos y periodos
+    </a>
+</template>
