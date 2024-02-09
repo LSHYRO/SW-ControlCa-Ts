c2abc7878de4a1f7e44e75b595651dae426418e2
Ernesto <ernestoalonsomartinezmartinez@gmail.com>
2024-02-09 13:25:30 -0600

    Camvios en correo

--- database/migrations/2023_09_09_205744_create_guardian_table.php
+++ database/migrations/2023_09_09_205744_create_guardian_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string(column: 'apellidoM')->nullable(false);
             $table->string(column: 'nombre')->nullable(false);
             $table->string(column: 'numTelefono')->nullable(false);
-            $table->string(column:'correoElectronico')->nullable(false);
+            $table->string(column:'correoElectronico')->nullable(true);
             $table->foreignId(column:'idGenero')->references('idGenero')->on('generos');
             $table->foreignId(column: 'idDireccion')->references('idDireccion')->on('direcciones');
             $table->foreignId(column: 'idUsuario')->references('idUsuario')->on('usuarios');
