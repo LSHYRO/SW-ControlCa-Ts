7b2c53dc77b851fd1eea21b8b081dece62ef229c
Juan Luis Lazaro Lopez <18161151@itoaxaca.edu.mx>
2024-02-14 20:26:27 -0600

    Fix: Barra superior(Logos)

--- resources/js/Components/director/TopContentDirec.vue
+++ resources/js/Components/director/TopContentDirec.vue
@@ -12,8 +12,8 @@ const props = defineProps({
 });
 
 const toggleSidebar = () => {
-            sideNav.classList.toggle('hidden'); // Agrega o quita la clase 'hidden' para mostrar u ocultar la navegación lateral
-        }
+    sideNav.classList.toggle('hidden'); // Agrega o quita la clase 'hidden' para mostrar u ocultar la navegación lateral
+}
 
 watchEffect(async () => {
     style.value = page.props.jetstream.flash?.bannerStyle || 'success';
@@ -23,27 +23,31 @@ watchEffect(async () => {
 </script>
 
 <template>
-<!-- Barra de navegación superior -->
-<div class="bg-cyan-600 text-white shadow w-full p-2 flex items-center justify-between">
-    <div class="flex items-center">
-        <div class="md:hidden flex items-center pe-2"> <!-- Se muestra solo en dispositivos pequeños -->
-            <button id="menuBtn" @click="toggleSidebar">
-                <i class="fas fa-bars text-white text-lg"></i> <!-- Ícono de menú -->
-            </button>
+    <div class="bg-cyan-600 text-white shadow w-full flex items-center justify-between md:max-h-12 h-16 rounded-sm">
+        <div class="flex items-center h-full">
+            <div class="md:hidden flex items-center pe-2 mx-1"> <!-- Se muestra solo en dispositivos pequeños -->
+                <button id="menuBtn" @click="toggleSidebar">
+                    <i class="fas fa-bars text-white text-lg"></i> <!-- Ícono de menú -->
+                </button>
+            </div>
+            <div class="h-full">
+                <img src="https://www.nvinoticias.com/sites/default/files/styles/normal_size/public/articulos/2023/Mayo/ieepo.png.webp?itok=lCgzpmJN"
+                    alt="IEEPO Logo" class="h-full md:mx-1 border-2 border-cyan-600 object-cover rounded-lg">
+            </div>
+            <div class="flex items-center justify-center text-center"> <!-- Mostrado en todos los dispositivos -->
+                <h1 class="md:font-bold md:text-2xl font-semibold text-xl">Telesecundaria clave: 20DTV1474D</h1>
+            </div>
         </div>
-        <div class="flex items-center"> <!-- Mostrado en todos los dispositivos -->
-            <h1 class="font-bold text-2xl">Telesecundaria clave: 20DTV1474D</h1>
+        <div class="flex items-center h-full">
+            <div class="flex items-center text-center h-full justify-center mx-1">
+                <i class="fas fa-user text-white font-thin mx-1"></i>
+                <i class="text-white font-['DM Sans'] mx-1"> {{ props.usuario.tipoUsuario1.charAt(0).toUpperCase() +
+                    props.usuario.tipoUsuario1.slice(1) }} </i>
+            </div>
+            <div class="h-full">
+                <img src="https://seeklogo.com/images/T/telesecundaria-logo-44765C9421-seeklogo.com.png"
+                    alt="Telesecundaria Logo" class="h-full md:mx-1 object-cover rounded-lg border-2 border-cyan-600">
+            </div>
         </div>
-
     </div>
-
-    <!-- Ícono de Notificación y Perfil -->
-    <div class="space-x-5">
-        <!-- Botón de Perfil -->
-        <div class="text-center">
-            <i class="fas fa-user text-white font-thin font-['DM Sans']"></i>
-            <i class="text-white font-['DM Sans'] m-3"> {{props.usuario.tipoUsuario1.charAt(0).toUpperCase() + props.usuario.tipoUsuario1.slice(1) }} </i>
-        </div>
-    </div>
-</div>
 </template>
